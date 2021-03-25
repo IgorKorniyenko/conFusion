86460deabaa0e60e5489a6e4c12c3a238de0ee75
Igor Korniyenko <igor.krnyk@gmail.com>
2021-03-25 20:01:12 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,10 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +28,11 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    AppRoutingModule
   ],
   imports: [
     BrowserModule,
