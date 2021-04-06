dedddc5c0d34145fa09340493b43e607532854fe
Igor Korniyenko <igor.krnyk@gmail.com>
2021-04-06 20:05:32 +0200

    dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,36 @@ import { MatCardModule} from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { DishService } from './services/dish.service';
+
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+import { PromotionService } from './services/promotion.service';
+import { LoginComponent } from './login/login.component';
+
+import { MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -29,10 +48,19 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule,
+    MatDialogModule
 
   ],
-  providers: [],
+  providers: [
+    DishService, 
+    PromotionService
+  ],
+  entryComponents:[
+    LoginComponent
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,101 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { Comment } from '../shared/comment';
 
+import { DishService } from '../services/dish.service';
 
-const COMMENTS: Comment[] = [
-  {
-    comm: 'Imagine all the eatables living in conFusion!',
-    puntuation: '5 Stars',
-    author: 'John Lenon',
-    date: 'Oct 17, 2012'
-  },
-  {
-    comm: 'Sends anyone to heaven, I wish I could get my mother-in-law',
-    puntuation: '4 Stars',
-    author: 'Paul MacVites',
-    date: 'Sep 6, 2014'
-  },
-  {
-    comm: 'Eat it, just eat it!',
-    puntuation: '3 Stars',
-    author: 'Michael Jakishan',
-    date: 'Feb 17, 2015'
-  },
-  {
-    comm: 'Ultimete, Reaching for the stars!',
-    puntuation: '4 Stars',
-    author: 'Ringo Starry',
-    date: 'Dec 3, 2013'
-  },
-  {
-    comm: 'Its your birthdat, were gonna party!',
-    puntuation: '2 Stars',
-    author: '25 Cent',
-    date: 'Dec 3, 2013'
-  }
-];
-const DISHES: Dish[] = [
-  {
-    id: '0',
-    name: 'Uthappizza',
-    image: '/assets/images/uthappizza.png',
-    category: 'mains',
-    featured: true,
-    label: 'Hot',
-    price: '4.99',
-    // tslint:disable-next-line:max-line-length
-    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-  },
-  {
-    id: '1',
-    name: 'Zucchipakoda',
-    image: '/assets/images/zucchipakoda.png',
-    category: 'appetizer',
-    featured: false,
-    label: '',
-    price: '1.99',
-    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-  },
-  {
-    id: '2',
-    name: 'Vadonut',
-    image: '/assets/images/vadonut.png',
-    category: 'appetizer',
-    featured: false,
-    label: 'New',
-    price: '1.99',
-    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-  },
-  {
-    id: '3',
-    name: 'ElaiCheese Cake',
-    image: '/assets/images/elaicheesecake.png',
-    category: 'dessert',
-    featured: false,
-    label: '',
-    price: '2.99',
-    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-  }
- ];
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
- 
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.css']
 })
+
+
 export class DishdetailComponent implements OnInit {
-
-  comments = COMMENTS;
-  dishes = DISHES;
-
-  selectedDish: Dish = DISHES[0];
-  constructor() { }
+  @Input()
+  dish: Dish;
+  
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
